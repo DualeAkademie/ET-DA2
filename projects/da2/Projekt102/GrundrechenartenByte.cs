37edd36b0d0e422be902497c86ff6310708c78ea
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2023-10-19 17:03:02 +0200

    -

--- projects/da2/Projekt102/GrundrechenartenByte.cs
+++ projects/da2/Projekt102/GrundrechenartenByte.cs
@@ -2,28 +2,10 @@ namespace Projekt102;
 
 public static class GrundrechenartenByte
 {
-    public static byte Addieren(byte summand1, byte summand2)
-    {
-        return 0;
-    }
+    public static byte Addieren(byte summand1, byte summand2) => (byte) (summand1 + summand2);
+    public static byte Subtrahieren(byte minuend, byte subtrahend) => (byte) (minuend - subtrahend);
+    public static byte Multiplizieren(byte multiplikand, byte multiplikator) => (byte) (multiplikand * multiplikator);
+    public static byte Dividieren(byte dividend, byte divisor) => (byte) (dividend / divisor);
+    public static byte RestBerechnen(byte a, byte b) => (byte) (a % b);
 
-    public static byte Subtrahieren(byte minuend, byte subtrahend)
-    {
-        return 0;
-    }
-
-    public static byte Multiplizieren(byte multiplikand, byte multiplikator)
-    {
-        return 0;
-    }
-
-    public static byte Dividieren(byte dividend, byte divisor)
-    {
-        return 0;
-    }
-
-    public static byte RestBerechnen(byte a, byte b)
-    {
-        return 0;
-    }
 }
